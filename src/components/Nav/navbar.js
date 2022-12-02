71e167be54d26558b7b6f071d8ac48be79af41ad
Facundo Gordillo <gordillofacundo44@gmail.com>
2022-12-02 11:40:58 -0300

    PreEntrega1-gordillo

--- src/components/Nav/navbar.js
+++ src/components/Nav/navbar.js
@@ -1,20 +1,37 @@
 function Navbar(){
     const navstyle = {
-        wheight: "100%",
+        width: "100%",
+        height: "3rem",
+        padding: "0",
+        margin: "0",
         backgroundColor: "black",
         color: "white",
-        display: "flex",
     };
+
+    const ul_nav_style = {
+        height: "100%",
+        display: "flex",
+        padding: "0",
+        margin: "0",
+        listStyle: 'none',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    }
+
+    const li_nav_style = {
+        paddingLeft: '1rem',
+    }
 
     return (
         <nav className='nav-element' style={navstyle}>
-            <ul>
-                <li>prueba</li>
-                <li>Contacto</li>
-                <li>Ubicacion</li>
+            <ul className='ul-nav' style={ul_nav_style}>
+                <li className='li-element-nav' style={li_nav_style}>Inicio</li>
+                <li className='li-element-nav' style={li_nav_style}>Contacto</li>
+                <li className='li-element-nav' style={li_nav_style}>Ubicacion</li>
             </ul>
         </nav>
     );
 }
 
-export default Navbar; 
+export default Navbar;
