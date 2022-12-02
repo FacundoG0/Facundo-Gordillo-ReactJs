f3dd59d7e64642dea6320ad920cd0ed436afb896
Facundo Gordillo <gordillofacundo44@gmail.com>
2022-12-02 10:57:02 -0300

    PreEntrega1-gordillo

---
+++ src/components/Nav/navbar.js
@@ -0,0 +1,19 @@
+function navbar(){
+    const navstyle = {
+        padding: "100%",
+        backgroundColor: "black",
+        color: "white"
+    };
+
+    return (
+        <nav classname='nav-element' style={navstyle}>
+            <ul>
+                <li>Inicio</li>
+                <li>Contacto</li>
+                <li>Mapa</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default navbar; 
