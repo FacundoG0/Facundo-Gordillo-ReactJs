f6b9f0073a5bfa25948b852a03219e072b3014fc
Facundo Gordillo <gordillofacundo44@gmail.com>
2022-12-02 11:58:23 -0300

    PreEntrega1-gordillo

--- src/components/Nav/navbar.js
+++ src/components/Nav/navbar.js
@@ -5,7 +5,6 @@ function Navbar(){
         padding: "0",
         margin: "0",
         backgroundColor: "black",
-        color: "white",
     };
 
     const ul_nav_style = {
@@ -16,19 +15,25 @@ function Navbar(){
         listStyle: 'none',
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
     }
 
     const li_nav_style = {
-        paddingLeft: '1rem',
+        padding: '0.2rem',
+        border: '1.5px solid white'
+    }
+
+    const a_nav_style = {
+        color: 'white',
+        fontWeight: 'bolder',
     }
 
     return (
         <nav className='nav-element' style={navstyle}>
             <ul className='ul-nav' style={ul_nav_style}>
-                <li className='li-element-nav' style={li_nav_style}>Inicio</li>
-                <li className='li-element-nav' style={li_nav_style}>Contacto</li>
-                <li className='li-element-nav' style={li_nav_style}>Ubicacion</li>
+                <li className='li-element-nav' style={li_nav_style}><a href='#' style={a_nav_style}>Inicio</a></li>
+                <li className='li-element-nav' style={li_nav_style}><a href='#' style={a_nav_style}>Contacto</a></li>
+                <li className='li-element-nav' style={li_nav_style}><a href='#' style={a_nav_style}>Ubicacion</a></li>
             </ul>
         </nav>
     );
